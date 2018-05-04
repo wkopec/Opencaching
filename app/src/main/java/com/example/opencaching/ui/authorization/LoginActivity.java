9b4edbd09bba32bb1554f3d4d54224b534876b28
Wojciech Kopeć <wojciech.koepc@ready4s.pl>
2018-05-04 22:41:45 +0200

    native login handle added

--- app/src/main/java/com/example/opencaching/ui/authorization/LoginActivity.java
+++ app/src/main/java/com/example/opencaching/ui/authorization/LoginActivity.java
@@ -3,14 +3,11 @@ package com.example.opencaching.ui.authorization;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.webkit.WebView;
 
 import com.example.opencaching.R;
 import com.example.opencaching.ui.base.BaseActivity;
 import com.example.opencaching.ui.main.MainActivity;
 import com.example.opencaching.ui.authorization.login.LoginFragment;
-
-import butterknife.BindView;
 
 import static com.example.opencaching.utils.UserUtils.isLoggedIn;
 
@@ -19,9 +16,6 @@ import static com.example.opencaching.utils.UserUtils.isLoggedIn;
  */
 
 public class LoginActivity extends BaseActivity {
-
-    @BindView(R.id.webView)
-    WebView webView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/example/opencaching/ui/authorization/login/LoginFragment.java
+++ app/src/main/java/com/example/opencaching/ui/authorization/login/LoginFragment.java
@@ -12,6 +12,10 @@ import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.opencaching.R;
 import com.example.opencaching.ui.authorization.LoginActivity;
@@ -19,6 +23,7 @@ import com.example.opencaching.ui.base.BaseFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.example.opencaching.utils.StringUtils.getOathToken;
 import static com.example.opencaching.utils.StringUtils.getOathVerifier;
@@ -29,8 +34,22 @@ import static com.example.opencaching.utils.StringUtils.getOathVerifier;
 
 public class LoginFragment extends BaseFragment implements LoginFragmentContract.View {
 
-    @BindView(R.id.webView)
-    WebView webView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+
+    @BindView(R.id.username)
+    EditText username;
+
+    @BindView(R.id.password)
+    EditText password;
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
+
+    @BindView(R.id.mask)
+    View mask;
+
+    private WebView webView;
     private LoginActivity activity;
     private LoginFragmentContract.Presenter presenter;
 
@@ -48,12 +67,16 @@ public class LoginFragment extends BaseFragment implements LoginFragmentContract
 
     @Override
     public void showProgress() {
-
+        loginButton.setClickable(false);
+        progressBar.setVisibility(View.VISIBLE);
+        mask.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideProgress() {
-
+        loginButton.setClickable(true);
+        progressBar.setVisibility(View.GONE);
+        mask.setVisibility(View.GONE);
     }
 
     @Override
@@ -78,22 +101,49 @@ public class LoginFragment extends BaseFragment implements LoginFragmentContract
         } else {
             cookieManager.removeAllCookie();
         }
-
-        //webView.setVisibility(View.VISIBLE);
+        webView = new WebView(activity);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new CustomWebViewClient());
     }
 
     private class CustomWebViewClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            if (url.contains("oauth_verifier=")) {
-                //webView.setVisibility(View.GONE);
+            if(url.contains("/login.php")) {
+                hideProgress();
+            } else if (url.contains("oauth_verifier=")) {
                 presenter.getOauthTokenSecret(getOathVerifier(url), getOathToken(url));
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if(url.contains("authorize?oauth_token")) {
+                acceptOauthApplicationAccess();
+            } else if(url.contains("https://opencaching.pl/UserAuthorization/login")) {
+                Toast.makeText(activity, getString(R.string.invalid_username_or_password), Toast.LENGTH_LONG).show();
+                hideProgress();
             }
         }
     }
 
+    @OnClick(R.id.loginButton)
+    public void onLoginButtonClick() {
+        showProgress();
+        webView.loadUrl("javascript: {" +
+                "document.getElementById('userName').value = '" + username.getText() + "';" +
+                "document.getElementById('password').value = '" + password.getText() + "';" +
+                "var frms = document.getElementsByName('login_form');" +
+                "frms[0].submit(); };");
+
+    }
+
+    private void acceptOauthApplicationAccess() {
+        webView.loadUrl("javascript:var x = document.getElementById('authform_result').setAttribute('value', 'granted'); document.forms['authform'].submit();");
+
+    }
 }
